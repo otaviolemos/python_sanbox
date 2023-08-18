3731d9944ea63d9a25d8b4c9c140bb69c3afeac9
Otavio Lemos <otaviolemos@gmail.com>
2023-08-18 10:45:36 -0300

    refactor

--- src/longest_palindrome/palindrome.py
+++ src/longest_palindrome/palindrome.py
@@ -1,15 +1,15 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        window = len(s)
-        for i in range(window, 1, -1):
+        strlen = len(s)
+        for window in range(strlen, 1, -1):
             start = 0
-            end = start + i-1
+            end = start + window-1
             while end <= len(s)-1:
                 currentString = s[start:end+1]
                 if isPalindrome(s[start:end+1]):
                     return currentString
                 start += 1
-                end = start + i-1
+                end = start + window-1
         return s[0]
                     
 
